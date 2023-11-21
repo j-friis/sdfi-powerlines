22397187ae1d5daf3581b722bd55ea081fd8c97b
j-friis <xdr476@alumni.ku.dk>
2023-11-21 12:45:48 +0100

    Fix: use correct env name

--- install/install_pdal_env.sh
+++ install/install_pdal_env.sh
@@ -1,5 +1,5 @@
-conda create -n pdal_test python=3.8 -y
+conda create -n pdal python=3.8 -y
 eval "$(conda shell.bash hook)"
-conda activate pdal_test
+conda activate pdal
 conda install -c conda-forge pdal python-pdal gdal
 pip install laspy[lazrs]
